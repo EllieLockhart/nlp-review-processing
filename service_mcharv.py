745a001187a5b471b86f2f821ec0adb9d5ea04c3
Ellie Lockhart <69021568+EllieLockhart@users.noreply.github.com>
2020-10-28 14:12:15 -0500

    added a lot of boilerplate (may want to move to docs once project is ready) and imports. Still functioning in the conda environment, will want to move to pip for beta

--- service_mcharv.py
+++ service_mcharv.py
@@ -20,3 +20,20 @@ import lxml
 # import client-side data handling
 import pandas as pd
 
+def user_input_base_url():
+    print('Service Script for Entertainment Marketing Data Analysis')
+    print('target site and purpose: Metacritic video game user reviews')
+    print('author: Ellie Lockhart, Ph.D.')
+    print('updated: 28 October 2020\n\n')
+    print('It is the user\'s responsiblity to make sure they are following all network rules and regulations related to automated data extraction on the platform on which they run the software. By entering a URL to scan, you understand that a large amount of data may be retrieved from a third party\'s server, and you warrant that you have permission from both the targeted server\'s operator and the operator of any server or other device from which you may be operating this script that this use case is acceptable.\n\n')
+    print('In order to extract the data from reviews, this service script requires a MetaCritic base game URL.')
+    print('These URLs consist of both a game\'s platform, and a web-friendly version of its title.')
+    print('An example of such a URL is: https://www.metacritic.com/game/xbox-360/tomb-raider')
+    print('Keep in mind that this will NOT extract any reviews for the PC, PlayStation 3, or PlayStation 4 versions of this title. Furthermore, the pandas dataframe and database integrations will store these reviews separately. If needed, you may adjust the database interface as appropriate if you wish to combine reviews of games across multiple platforms.\n\n')
+    print('OK! Let\'s get to work: please copy and paste a relevant Metacritic base URL, assuming you agree to the consent affirmation above as well as the license terms under which this script is distributed.\n')
+    input_url = input('Base URL (Metacritic only):')
+    # pass the URL out of the function so it can be used elsewhere
+    return input_url
+
+
+
